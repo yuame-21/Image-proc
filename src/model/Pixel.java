53c05072f541b845c8860ea2f5078b8a4061962a
etaira <107139964+e-taira@users.noreply.github.com>
2022-06-16 18:41:42 -0400

    pixel af

--- src/model/Pixel.java
+++ src/model/Pixel.java
@@ -1,35 +1,69 @@
 package model;
 
+/**
+ * Represents a color Pixel made up of a red, green, and blue value.
+ */
 public class Pixel {
   int red;
   int green;
   int blue;
 
+  /**
+   * A color pixel consists of red, green, and blue integer values.
+   * All color values must be <= 255
+   *
+   * @param red   integer red value
+   * @param green integer green value
+   * @param blue  integer blue value
+   */
   public Pixel(int red, int green, int blue) {
     this.red = red;
     this.green = green;
     this.blue = blue;
   }
 
-  public int get(int i) {
+  /**
+   * Retrieves the color integer value based on given integer.
+   * 0  = red (the first color)
+   * 1 = green (the second color)
+   * 2 = blue (the third color)
+   *
+   * @param i integer "position" of color value to get.
+   * @return integer color value
+   */
+  public int get(int i) throws IllegalArgumentException {
     int c;
     switch (i) {
       case 0:
-        c= this.red;
-      break;
+        c = this.red;
+        break;
       case 1:
-        c= this.green;
-      break;
+        c = this.green;
+        break;
       case 2:
-        c= this.blue;
-      break;
+        c = this.blue;
+        break;
       default:
         throw new IllegalArgumentException("invalid color index");
     }
     return c;
   }
 
-  public void set(int i, int set) {
+  /**
+   * Sets an individual color value (R,G, or B) to a given value based on location input.
+   * i = 0  -> red (the first color)
+   * i = 1 -> green (the second color)
+   * i = 2 -> blue (the third color)
+   *
+   * @param i   which color to set.
+   * @param set integer value to the set the color value to
+   */
+  public void set(int i, int set) throws IllegalArgumentException {
+
+    if (set > 255) {
+      throw new IllegalArgumentException("color value may be at most 255");
+    }
+
     switch (i) {
       case 0:
         this.red = set;
@@ -43,7 +77,11 @@ public class Pixel {
     }
   }
 
-
+  /**
+   * Returns the R,G, and B values of the pixel in the form of a three-integer Array.
+   *
+   * @return array of length 3
+   */
   public int[] getChannel() {
     return new int[]{this.red, this.green, this.blue};
   }
---
+++ test/PixelTest.java
@@ -0,0 +1,62 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import model.Pixel;
+
+import static org.junit.Assert.*;
+
+public class PixelTest {
+  Pixel p1;
+  Pixel p2;
+
+  @Before
+  public void init() {
+    this.p1 = new Pixel(1, 2, 3);
+    this.p2 = new Pixel(100, 200, 0);
+  }
+
+  @Test
+  public void get() {
+    assertEquals(1, this.p1.get(0));
+    assertEquals(2, this.p1.get(1));
+    assertEquals(3, this.p1.get(2));
+    assertEquals(100, this.p2.get(0));
+    assertEquals(200, this.p2.get(1));
+    assertEquals(0, this.p2.get(2));
+
+    try {
+      this.p1.get(5);
+      fail("should have thrown illegal arg exc");
+    } catch (IllegalArgumentException e) {
+      if (!e.getMessage().equals("invalid color index")) {
+        fail("should have thrown illegal arg exc: invalid color index");
+      }
+    }
+  }
+
+  @Test
+  public void set() {
+    this.p1.set(0, 100);
+    assertEquals(100, this.p1.get(0));
+
+    try {
+      this.p1.set(0, 500);
+      fail("should have thrown illegal arg exc");
+    } catch (IllegalArgumentException e) {
+      if (!e.getMessage().equals("color value may be at most 255")) {
+        fail("should have thrown illegal arg exc : color value may be at most 255");
+      }
+    }
+
+    this.p1.set(1, 11);
+    assertEquals(11, this.p1.get(1));
+    this.p1.set(2, 200);
+    assertEquals(200, this.p1.get(2));
+  }
+
+  @Test
+  public void getChannel() {
+    assertArrayEquals(new int[]{1, 2, 3}, this.p1.getChannel());
+    assertArrayEquals(new int[]{100, 200, 0}, this.p2.getChannel());
+  }
+}
