375a3a74c21c6fb4729db241db0155edcc0c2678
yuame-21 <yu.ame@northeastern.edu>
2022-06-23 09:48:55 -0400

    fixing histogram still

--- src/model/ImageProcessorHistogram.java
+++ src/model/ImageProcessorHistogram.java
@@ -5,7 +5,7 @@ package model;
  * This class creates arrays of each histogram field from a given ImageModelState.
  */
 public class ImageProcessorHistogram {
-  private ImageModelState image;
+  private ImageModel image;
   private int[] reds;
   private int[] greens;
   private int[] blues;
@@ -17,12 +17,14 @@ public class ImageProcessorHistogram {
    *
    * @param image Image Model State that supplies data to make a histogram
    */
-  public ImageProcessorHistogram(ImageModelState image) {
+  public ImageProcessorHistogram(ImageModel image) {
     this.image = image;
-    this.reds = this.histogram("red");
-    this.greens = this.histogram("green");
-    this.blues = this.histogram("blue");
-    this.intensities = this.histogram("intensity");
+    this.histogram(image.generateString("temp"));
+
+//    this.reds = this.histogram("red");
+//    this.greens = this.histogram("green");
+//    this.blues = this.histogram("blue");
+//    this.intensities = this.histogram("intensity");
   }
 
   /**
@@ -35,38 +37,27 @@ public class ImageProcessorHistogram {
    * @throws IllegalArgumentException if histogram String type is invalid
    */
   private int[] histogram(String type) throws IllegalArgumentException {
-    int arrSize = this.image.getWidth() * this.image.getHeight();
+    String[] ar = type.split("\n");
+    int width = Integer.parseInt(ar[0]);
+    int height = Integer.parseInt(ar[1]);
+
+    int arrSize = width * height;
+
     int[] histogram = new int[arrSize];
     int a = 0;
+    int c = 2;
 
-    for (int i = 0; i < this.image.getHeight(); i++) {
-      for (int j = 0; j < this.image.getWidth(); j++) {
-        switch (type) {
-          case "red":
-            histogram[a] = this.image.getPixel(i, j).get(0);
-            break;
-          case "green":
-            histogram[a] = this.image.getPixel(i, j).get(1);
-            break;
-          case "blue":
-            histogram[a] = this.image.getPixel(i, j).get(2);
-            break;
-          case "intensity":
-            int[] channel = this.image.getPixel(i, j).getChannel();
-            int sum = 0;
-            for (int c = 0; c < 3; c++) {
-              sum += channel[c];
-            }
-            int intensity = (int) Math.round(sum / 3);
-            histogram[a] = intensity;
-            break;
-          default:
-            throw new IllegalArgumentException("Invalid histogram value: " +
-                    "must be red, green, blue, or intensity");
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        int sum = 0;
+        for(c = c; c < c + 3; c++) {
+          histogram[a] = Integer.parseInt(ar[c]);
+          sum += Integer.parseInt(ar[c]);
         }
-        a++;
+        int intensity = Math.round(sum / 3);
+        histogram[a] = intensity;
       }
-
+      a++;
     }
     return histogram;
   }
